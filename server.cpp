e2d30eaa422d22953740c45452d160c929bf9cf1
fmaldonados <felixmaldonado@unitec.edu>
2018-02-17 23:20:33 -0600

    ya funciona lo de servir archivos, solo quitaba librerias que no ocupaba

--- server.cpp
+++ server.cpp
@@ -12,18 +12,6 @@
 #include <vector>
 #include<fcntl.h>
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netdb.h>
-#include<signal.h>
-#include<fcntl.h>
-
 using namespace std;
 
 #define connects 1000
