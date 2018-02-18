d6615d1ba5c1d772fcf6cef8aacc9bcbc5a238d3
Cesar Figueroa <cesarabelfigueroa@gmail.com>
2018-02-17 18:20:39 -0600

    multithread support

--- server.cpp
+++ server.cpp
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <math.h>
+#include <vector>
 
 
 using namespace std;
@@ -19,19 +20,21 @@ void start(int &server);
 void *request(void *parameters);
 
 int main(int argc, char const *argv[]){
+	vector<pthread_t> pool; 
+	int bufSize = 1024; 
+	char buffer[bufSize]; 
 	int yes = 1;
-	int counter = 1;
+	int connections = 0;
 	int server, client; // socket file descriptors
 	int portNum = 8080; // port number
-	int bufSize = 1024; // buffer size
-	char buffer[bufSize]; // buffer to transmit
-	bool isExit = false; // var fo continue infinitly
+	bool isExit = true; // var fo continue infinitly
 	struct sockaddr_in server_addr;
 	socklen_t size;
 	start(server);
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htons(INADDR_ANY);
 	server_addr.sin_port = htons(portNum);
+
 	
 	if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
 		perror("setsockopt");
@@ -46,43 +49,20 @@ int main(int argc, char const *argv[]){
 	size = sizeof(server_addr);
 	cout << "- Looking for clients..." << endl;
 	listen(server, 1);
-	client = accept(server, (struct sockaddr *) &server_addr, &size);
 
-	cout << client << endl;
+	
+	while (client = accept(server, (struct sockaddr *) &server_addr, &size)) {
+		pthread_t thread;
+		recv(client, buffer, bufSize, 0);
+		if(client){
+			pthread_create(&thread, NULL, request, (void*) &client);
+			pool.push_back(thread);
+			connections++;
+		}
 
-	if (client < 0){
-		cout << "- Error on accepting..." << endl;
-	}
-
-	string echo;
-	while (client > 0) {
-		strcpy(buffer, "\n-> Welcome to echo server...\n");
-		send(client, buffer, bufSize, 0);
-		cout << "- Connected with the client, waiting for data..." << endl;
-		do {
-			cout << "\nClient: ";
-			echo = "";
-			do {
-				recv(client, buffer, bufSize, 0);
-				cout << buffer << " ";
-				if (*buffer == '#') {
-					isExit = true;
-					*buffer = '*';
-					echo = "Goodbye!";
-				} else if ((*buffer != '#') && (*buffer != '*')) {
-					echo += buffer;
-					echo += " ";
-				}
-				cout << echo.c_str() << endl;
-			} while (*buffer != '*');
-
-			// sprintf(buffer, "%s", echo.c_str());
-			// send(client, buffer, bufSize, 0);
-		} while (!isExit);
-		cout << "\n\n=> Connection terminated with IP " << inet_ntoa(server_addr.sin_addr);
-		close(client);
-		cout << "\nGoodbye..." << endl;
-		exit(1);
+		if (client < 0){
+			cout << "- Error on connecting..." << endl;
+		}
 	}
 
 	close(server);
@@ -100,6 +80,11 @@ void start(int &server){
 	cout << "- Socket server has been created..." << endl;
 }
 
-void *request(void *parameters){
-
+void *request(void *client){
+	int bufSize = 1024; 
+	char buffer[bufSize]; 
+	int connection = *((int*) client);
+	strcpy(buffer, "\n-> Welcome to echo server...\n");
+	send(connection, buffer, bufSize, 0);
+	cout << "Se conecto un cliente" <<endl;
 }
