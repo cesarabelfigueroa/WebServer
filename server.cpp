3bec9fe29e7d36ead77114dc968da04445fb9cc0
fmaldonados <felixmaldonado@unitec.edu>
2018-02-17 22:17:02 -0600

    probando response del server

--- server.cpp
+++ server.cpp
@@ -10,10 +10,13 @@
 #include <pthread.h>
 #include <math.h>
 #include <vector>
+#include<fcntl.h>
 
 
 using namespace std;
 
+#define connects 1000
+#define bufSize 1024
 
 
 void start(int &server);
@@ -21,7 +24,7 @@ void *request(void *parameters);
 
 int main(int argc, char const *argv[]){
 	vector<pthread_t> pool; 
-	int bufSize = 1024; 
+	//int bufSize = 1024; 
 	char buffer[bufSize]; 
 	int yes = 1;
 	int connections = 0;
@@ -81,10 +84,61 @@ void start(int &server){
 }
 
 void *request(void *client){
-	int bufSize = 1024; 
-	char buffer[bufSize]; 
-	int connection = *((int*) client);
-	strcpy(buffer, "\n-> Welcome to echo server...\n");
-	send(connection, buffer, bufSize, 0);
-	cout << "Se conecto un cliente" <<endl;
+	//int bufSize = 1024; 
+	//char buffer[bufSize]; 
+	//int connection = *((int*) client);
+	//strcpy(buffer, "\n-> Welcome to echo server...\n");
+	//send(connection, buffer, bufSize, 0);
+	//cout << "Se conecto un cliente" <<endl;
+
+	int n = *((int *) client);
+	char mesg[99999], *reqline[3], data_to_send[bufSize], path[99999];
+	int rcvd, fd, bytes_read;
+	
+	memset( (void*)mesg, (int)'\0', 99999 );
+	
+	rcvd=recv(n, mesg, 99999, 0);
+	
+	if (rcvd<0)    // receive error
+		fprintf(stderr,("recv() error\n"));
+	else if (rcvd==0)    // receive socket closed
+		fprintf(stderr,"Client disconnected upexpectedly.\n");
+	else    // message received
+	{
+		cout << "1" <<endl;
+		printf("%s", mesg);
+		reqline[0] = strtok (mesg, " \t\n");
+		if ( strncmp(reqline[0], "GET\0", 4)==0 )
+		{
+			reqline[1] = strtok (NULL, " \t");
+			reqline[2] = strtok (NULL, " \t\n");
+			if ( strncmp( reqline[2], "HTTP/1.0", 8)!=0 && strncmp( reqline[2], "HTTP/1.1", 8)!=0 )
+			{
+				write(n, "HTTP/1.0 400 Bad Request\n", 25);
+			}
+			else
+			{
+				if ( strncmp(reqline[1], "/\0", 2)==0 )
+					reqline[1] = "/index.html";        //Because if no file is specified, index.html will be opened by default (like it happens in APACHE...
+
+				strcpy(path, "./public");
+				strcpy(&path[strlen("./public")], reqline[1]);
+				printf("file: %s\n", path);
+
+				if ( (fd=open(path, O_RDONLY))!=-1 )    //FILE FOUND
+				{
+					send(n, "HTTP/1.0 200 OK\n\n", 17, 0);
+					cout << "Se conecto un cliente" <<endl;
+					while ( (bytes_read=read(fd, data_to_send, bufSize))>0 )
+						write (n, data_to_send, bytes_read);
+				}
+				else    write(n, "HTTP/1.0 404 Not Found\n", 23); //FILE NOT FOUND
+			}
+		}
+	}
+	cout << "exit" <<endl;
+	//Closing SOCKET
+	//shutdown (n, SHUT_RDWR);         //All further send and recieve operations are DISABLED...
+	//close(n);
+	//n=-1;
 }
